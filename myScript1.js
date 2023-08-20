959f3466518e596678acbd9045d04a0d9532dbe9
Tomislav Blazevic <tekennster@gmail.com>
2023-08-20 10:32:15 +0200

    updated with contra hire link

---
+++ myScript1.js
@@ -0,0 +1,6 @@
+
+const d = new Date();
+document.getElementById("time").innerHTML = d;
+
+
+
---
+++ myScript2.js
@@ -0,0 +1,12 @@
+
+const hour = new Date().getHours();
+let greeting;
+
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+document.getElementById("good day-evening").innerHTML = greeting;
+
